3d203e467ad6fea02f58326615b34d9c4a26d535
vickykum <vickykum@buffalo.edu>
2018-10-22 23:10:55 -0400

    Update Cards.java

--- src/PokerGame_GUI/Cards.java
+++ src/PokerGame_GUI/Cards.java
@@ -6,15 +6,28 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Cards {
 	
-	private char[] _suitName = {'C', 'D', 'H', 'S'};
-	private char[] _cardNum = {2,3,4,5,6,7,8,9,10,'J','K','Q','A'};
+	private String[] _suitName = {"C", "D", "H", "S"};
+	private String[] _cardNum = {"2","3","4","5","6","7","8","9","10","J","K","Q","A"};
+	private ArrayList<String> _allCards;
+	
+	public Cards() {
+		 ArrayList<String> AllCards = new ArrayList<String>();
+		for(int f = 0; f<_cardNum.length; f++ ) {
+			for(int s =0; s<_suitName.length; s++) {
+				AllCards.add( _cardNum[f] + _suitName[s]);
+			}
+		}
+		_allCards = AllCards;
+	}
 	
 	public BufferedImage projectImage(String getCard) {
-		String pngfile = "C:\\Users\\kotin\\Desktop\\cse 442\\CSE442_PokerGame\\src\\Card_PNG\\"+ getCard +".png";
+		String pngfile = "src\\Card_PNG\\"+ getCard +".png";
 		try {
 			BufferedImage bg = ImageIO.read(new File(pngfile));
 			
@@ -26,12 +39,22 @@ public class Cards {
 		}
 	}
 	
-	public ImageIcon getImageIcon(String getCard) {
-		ImageIcon Img = new ImageIcon(projectImage(getCard));
+	public ImageIcon getImageIcon() {
+		ImageIcon Img = new ImageIcon(projectImage(getRandCard()));
 		Image nIg = Img.getImage();
 		Image aginNew = nIg.getScaledInstance(100, 150, Image.SCALE_SMOOTH);
 		ImageIcon newImg = new ImageIcon(aginNew);
 		return newImg;
+	}
+	
+	public String getRandCard() {
+		Random rand = new Random();
+		int elem = rand.nextInt(_allCards.size());
+		System.out.println(elem);
+		String retval = "";
+		retval = 	_allCards.remove(elem);	
+		System.out.println(retval);
+		return retval;
 	}
 
 }
