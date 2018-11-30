6843d030c65763d2f1ecfd19cb2ba4f7e3bc8b68
vickykum <vickykum@buffalo.edu>
2018-11-29 20:01:49 -0500

    Update PokerDesign.java

--- src/PokerGame_GUI/PokerDesign.java
+++ src/PokerGame_GUI/PokerDesign.java
@@ -15,7 +15,7 @@ public class PokerDesign {
 
 	public PokerDesign() {
 		JFrame guiFrame = new JFrame();
-		guiFrame.getContentPane().setBackground(new Color(135, 206, 250));
+		guiFrame.getContentPane().setBackground(Color.WHITE);
 		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		guiFrame.setTitle("Example GUI");
 		guiFrame.setSize(700, 700);
@@ -31,7 +31,7 @@ public class PokerDesign {
 
 		// The first JPanel contains a JLabel and JCombobox
 		final JPanel comboPanel = new JPanel(new GridBagLayout());
-		comboPanel.setBackground(Color.cyan);
+		comboPanel.setBackground(Color.WHITE);
 
 		JLabel welcomeLabl = new JLabel("THE WINGS POKER");
 		welcomeLabl.setFont(new Font("Segoe Print", Font.PLAIN, 19));
@@ -58,10 +58,9 @@ public class PokerDesign {
 		gbc_listLbl.gridx = 0;
 		gbc_listLbl.gridy = 0;
 		listPanel.add(listLbl, gbc_listLbl);
-		
-				JButton BACKButton = new JButton("BACK");
-				BACKButton.setBackground(Color.RED);
-				
+
+		JButton BACKButton = new JButton("BACK");
+		BACKButton.setBackground(Color.RED);
 
 		lc.gridx = 0;
 		lc.gridy = 1;
@@ -73,14 +72,55 @@ public class PokerDesign {
 		p2c.gridx = 20;
 		p2c.gridy = 20;
 
-		JPanel picPanel = new JPanel(new GridBagLayout());
+		GridBagLayout gbl_picPanel = new GridBagLayout();
+		gbl_picPanel.rowWeights = new double[] {};
+		gbl_picPanel.columnWeights = new double[] {};
+		JPanel picPanel = new JPanel(gbl_picPanel);
+		picPanel.setBackground(Color.WHITE);
 		picPanel.setVisible(false);
+
+		JPanel subPicpanel = new JPanel();
+		GridBagConstraints gbc_subPicpanel = new GridBagConstraints();
+		gbc_subPicpanel.fill = GridBagConstraints.BOTH;
+		gbc_subPicpanel.gridx = 0;
+		gbc_subPicpanel.gridy = 0;
+		picPanel.add(subPicpanel, gbc_subPicpanel);
+		GridBagLayout gbl_subPicpanel = new GridBagLayout();
+		gbl_subPicpanel.columnWidths = new int[] { 539, 0 };
+		gbl_subPicpanel.rowHeights = new int[] { 100, 100, 100, 0 };
+		gbl_subPicpanel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
+		gbl_subPicpanel.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		subPicpanel.setLayout(gbl_subPicpanel);
+
+		JPanel p1cards = new JPanel();
+		GridBagConstraints gbc_p1cards = new GridBagConstraints();
+		gbc_p1cards.fill = GridBagConstraints.BOTH;
+		gbc_p1cards.insets = new Insets(0, 0, 5, 0);
+		gbc_p1cards.gridx = 0;
+		gbc_p1cards.gridy = 0;
+		subPicpanel.add(p1cards, gbc_p1cards);
+		FlowLayout fl_p1cards = new FlowLayout(FlowLayout.CENTER, 5, 5);
+		p1cards.setLayout(fl_p1cards);
+
+		JPanel p2cards = new JPanel();
+		GridBagConstraints gbc_p2cards = new GridBagConstraints();
+		gbc_p2cards.fill = GridBagConstraints.BOTH;
+		gbc_p2cards.insets = new Insets(0, 0, 5, 0);
+		gbc_p2cards.gridx = 0;
+		gbc_p2cards.gridy = 1;
+		subPicpanel.add(p2cards, gbc_p2cards);
+		FlowLayout fl_p2cards = new FlowLayout(FlowLayout.CENTER, 5, 5);
+		p2cards.setLayout(fl_p2cards);
 
 		Cards cards = new Cards();
 		/* These labels are the back side of cards with The wings LOGO */
 		JLabel[][] label = cards.gameCardsLabel();
-		for (int i = 0; i < label.length; i++) {
-			picPanel.add(label[i][0]);
+		for (int i = 0; i < 5; i++) {
+			p1cards.add(label[i][0]);// player 1 cards
+			if (i + 5 > 7) {
+				p2cards.add(label[i + 5][1]);// player 2 cards front faced
+			} else
+				p2cards.add(label[i + 5][0]);// player 2 cards
 		}
 
 		/* These labels are the face side of the card that will be updated later */
@@ -93,97 +133,93 @@ public class PokerDesign {
 			player_two_names.add(label_names.get(i + 5));
 		}
 
-
 		final JPanel evaluateButtonPanel = new JPanel(new GridBagLayout());
 		evaluateButtonPanel.setVisible(false);
-						
-								final JPanel p2WinsPanel = new JPanel(new GridBagLayout());
-								GridBagConstraints gbc_p2WinsPanel = new GridBagConstraints();
-								gbc_p2WinsPanel.insets = new Insets(0, 0, 5, 5);
-								gbc_p2WinsPanel.gridx = 0;
-								gbc_p2WinsPanel.gridy = 0;
-								JLabel p2Wins = new JLabel("PLAYER 2 WINS!!!");
-								GridBagConstraints gbc_p2Wins = new GridBagConstraints();
-								p2WinsPanel.add(p2Wins, gbc_p2Wins);
-								p2WinsPanel.setVisible(false);
-								evaluateButtonPanel.add(p2WinsPanel, gbc_p2WinsPanel);
-		
+
+		final JPanel p2WinsPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc_p2WinsPanel = new GridBagConstraints();
+		gbc_p2WinsPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_p2WinsPanel.gridx = 0;
+		gbc_p2WinsPanel.gridy = 0;
+		JLabel p2Wins = new JLabel("YOU WON THE DEAL!!!");
+		GridBagConstraints gbc_p2Wins = new GridBagConstraints();
+		p2WinsPanel.add(p2Wins, gbc_p2Wins);
+		p2WinsPanel.setVisible(false);
+		evaluateButtonPanel.add(p2WinsPanel, gbc_p2WinsPanel);
+
 		final JPanel p1WinsPanel = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc_p1WinsPanel = new GridBagConstraints();
-		gbc_p1WinsPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_p1WinsPanel.insets = new Insets(0, 0, 5, 0);
 		gbc_p1WinsPanel.gridx = 0;
 		gbc_p1WinsPanel.gridy = 1;
 		JLabel p1Wins = new JLabel("PLAYER 1 WINS!!!");
 		p1WinsPanel.add(p1Wins);
 		p1WinsPanel.setVisible(false);
 		evaluateButtonPanel.add(p1WinsPanel, gbc_p1WinsPanel);
-		
-				JButton evaluateButton = new JButton("EVALUATE");
-				
-						evaluateButton.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent event) {
-								if (highCard(player_one_names) > highCard(player_two_names)) {
-									for(int i =0; i<evaluateButtonPanel.getComponentCount();i++) {
-										if(i == 1 || i==4) {//player location and QUIT button
-											evaluateButtonPanel.getComponent(i).setVisible(true);
-										}
-										else {
-											evaluateButtonPanel.getComponent(i).setVisible(false);
-										}
-									}
-									comboPanel.setVisible(false);
-									picPanel.setVisible(true);
-									listPanel.setVisible(false);
-									evaluateButtonPanel.setVisible(true);
-				
-								} else {
-									for(int i =0; i<evaluateButtonPanel.getComponentCount();i++) {
-										if(i == 0 || i==4) {//player location and QUIT button
-											evaluateButtonPanel.getComponent(i).setVisible(true);
-										}
-										else {
-											evaluateButtonPanel.getComponent(i).setVisible(false);
-										}
-									}
-									comboPanel.setVisible(false);
-									picPanel.setVisible(true);
-									listPanel.setVisible(false);
-									evaluateButtonPanel.setVisible(true);
-									
-								}
-							}
-						});
-						
-						ActionListener quitAction = new ActionListener() {
-							public void actionPerformed(ActionEvent event) {
-								picPanel.setVisible(false);
-								listPanel.setVisible(false);
-								evaluateButtonPanel.setVisible(false);
-								p2WinsPanel.setVisible(false);
-								p1WinsPanel.setVisible(false);
-								comboPanel.setVisible(true);
-							}
-						};
-						
-						BACKButton.addActionListener(quitAction);
-						GridBagConstraints gbc_BACKButton = new GridBagConstraints();
-						gbc_BACKButton.insets = new Insets(0, 0, 5, 0);
-						gbc_BACKButton.gridx = 2;
-						gbc_BACKButton.gridy = 0;
-						listPanel.add(BACKButton, gbc_BACKButton);
-						
-						GridBagConstraints gbc_evaluateButton = new GridBagConstraints();
-						gbc_evaluateButton.insets = new Insets(0, 0, 5, 5);
-						gbc_evaluateButton.gridx = 0;
-						gbc_evaluateButton.gridy = 2;
-						evaluateButtonPanel.add(evaluateButton, gbc_evaluateButton);
 
-		evaluateButtonPanel.setBackground(Color.cyan);
+		JButton evaluateButton = new JButton("EVALUATE");
+
+		evaluateButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				if (highCard(player_one_names) > highCard(player_two_names)) {
+					for (int i = 0; i < evaluateButtonPanel.getComponentCount(); i++) {
+						if (i == 1 || i == 4) {// player location and QUIT button
+							evaluateButtonPanel.getComponent(i).setVisible(true);
+						} else {
+							evaluateButtonPanel.getComponent(i).setVisible(false);
+						}
+					}
+					comboPanel.setVisible(false);
+					picPanel.setVisible(true);
+					listPanel.setVisible(false);
+					evaluateButtonPanel.setVisible(true);
+
+				} else {
+					for (int i = 0; i < evaluateButtonPanel.getComponentCount(); i++) {
+						if (i == 0 || i == 4) {// player location and QUIT button
+							evaluateButtonPanel.getComponent(i).setVisible(true);
+						} else {
+							evaluateButtonPanel.getComponent(i).setVisible(false);
+						}
+					}
+					comboPanel.setVisible(false);
+					picPanel.setVisible(true);
+					listPanel.setVisible(false);
+					evaluateButtonPanel.setVisible(true);
+
+				}
+			}
+		});
+
+		ActionListener quitAction = new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				picPanel.setVisible(false);
+				listPanel.setVisible(false);
+				evaluateButtonPanel.setVisible(false);
+				comboPanel.setVisible(true);
+			}
+		};
+
+		BACKButton.addActionListener(quitAction);
+		GridBagConstraints gbc_BACKButton = new GridBagConstraints();
+		gbc_BACKButton.insets = new Insets(0, 0, 5, 0);
+		gbc_BACKButton.gridx = 2;
+		gbc_BACKButton.gridy = 0;
+		listPanel.add(BACKButton, gbc_BACKButton);
+
+		GridBagConstraints gbc_evaluateButton = new GridBagConstraints();
+		gbc_evaluateButton.insets = new Insets(0, 0, 5, 0);
+		gbc_evaluateButton.gridx = 0;
+		gbc_evaluateButton.gridy = 2;
+		evaluateButtonPanel.add(evaluateButton, gbc_evaluateButton);
+
+		evaluateButtonPanel.setBackground(Color.WHITE);
 
 		GridLayout grid = new GridLayout(2, 5, 2, 2);
 		picPanel.setLayout(grid);
+		guiFrame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
-		guiFrame.getContentPane().add(comboPanel, BorderLayout.NORTH);
+		guiFrame.getContentPane().add(comboPanel);
 		JButton btnStartGame = new JButton("START GAME");
 		btnStartGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -199,7 +235,7 @@ public class PokerDesign {
 		gbc_btnStartGame.gridy = 1;
 		comboPanel.add(btnStartGame, gbc_btnStartGame);
 
-		JButton btnHowToPlay = new JButton("HOW TO PLAY");
+		JButton btnHowToPlay = new JButton("ABOUT GAME");
 		btnHowToPlay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				comboPanel.setVisible(false);
@@ -213,54 +249,96 @@ public class PokerDesign {
 		gbc_btnHowToPlay.gridy = 1;
 		comboPanel.add(btnHowToPlay, gbc_btnHowToPlay);
 
+		JPanel moneyPanel = new JPanel();
+		guiFrame.getContentPane().add(moneyPanel);
+
+		JLabel boardLabel = new JLabel("MONEYBOARD");
+		moneyPanel.add(boardLabel);
+		moneyPanel.setVisible(false);
+
 		JButton btnMoneyboard = new JButton("MONEYBOARD");
+		btnMoneyboard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				picPanel.setVisible(false);
+				listPanel.setVisible(false);
+				evaluateButtonPanel.setVisible(false);
+				comboPanel.setVisible(false);
+				moneyPanel.setVisible(true);
+			}
+		});
 		GridBagConstraints gbc_btnMoneyboard = new GridBagConstraints();
 		gbc_btnMoneyboard.insets = new Insets(0, 0, 0, 5);
 		gbc_btnMoneyboard.gridx = 2;
 		gbc_btnMoneyboard.gridy = 1;
 		comboPanel.add(btnMoneyboard, gbc_btnMoneyboard);
-				
-						JButton btnShop = new JButton("NEW GAME");
-						btnShop.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent e) {
-							}
-						});
-						GridBagConstraints gbc_btnShop = new GridBagConstraints();
-						gbc_btnShop.gridx = 5;
-						gbc_btnShop.gridy = 1;
-						comboPanel.add(btnShop, gbc_btnShop);
 
-		guiFrame.getContentPane().add(evaluateButtonPanel, BorderLayout.WEST);
-		
-				GridBagConstraints evbc = new GridBagConstraints();
-				evbc.insets = new Insets(0, 0, 5, 5);
-				evbc.gridx = 0;
-				evbc.gridy = 3;
-				
-						JLabel evLbl = new JLabel("PRESS TO EVALUATE:");
-						evLbl.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-						
-								evaluateButtonPanel.add(evLbl, evbc);
-								evbc.gridx = 0;
-								evbc.gridy = 0;
-								
-										
-								
-										evbc.gridx = 1;
-										evbc.gridy = 1;
-										evbc.gridx = 1;
-										evbc.gridy = 2;
-										JButton QUITButton = new JButton("QUIT-GAME");
-										QUITButton.setBackground(Color.RED);
-										GridBagConstraints gbc_QUITButton = new GridBagConstraints();
-										gbc_QUITButton.insets = new Insets(0, 0, 0, 5);
-										gbc_QUITButton.gridx = 0;
-										gbc_QUITButton.gridy = 4;
-										QUITButton.addActionListener(quitAction);
-										evaluateButtonPanel.add(QUITButton, gbc_QUITButton);
+		JButton btnShop = new JButton("NEW GAME");
+		btnShop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnShop = new GridBagConstraints();
+		gbc_btnShop.gridx = 5;
+		gbc_btnShop.gridy = 1;
+		comboPanel.add(btnShop, gbc_btnShop);
 
-		guiFrame.getContentPane().add(picPanel, BorderLayout.AFTER_LINE_ENDS);
-		guiFrame.getContentPane().add(listPanel, BorderLayout.SOUTH);
+		guiFrame.getContentPane().add(evaluateButtonPanel);
+
+		GridBagConstraints evbc = new GridBagConstraints();
+		evbc.insets = new Insets(0, 0, 5, 0);
+		evbc.gridx = 0;
+		evbc.gridy = 3;
+
+		JLabel evLbl = new JLabel("PRESS TO EVALUATE:");
+		evLbl.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+
+		evaluateButtonPanel.add(evLbl, evbc);
+		evbc.gridx = 0;
+		evbc.gridy = 0;
+
+		evbc.gridx = 1;
+		evbc.gridy = 1;
+		evbc.gridx = 1;
+		evbc.gridy = 2;
+		JButton QUITButton = new JButton("QUIT-GAME");
+		QUITButton.setBackground(Color.RED);
+		GridBagConstraints gbc_QUITButton = new GridBagConstraints();
+		gbc_QUITButton.insets = new Insets(0, 0, 5, 0);
+		gbc_QUITButton.gridx = 0;
+		gbc_QUITButton.gridy = 4;
+		QUITButton.addActionListener(quitAction);
+		evaluateButtonPanel.add(QUITButton, gbc_QUITButton);
+
+		JButton btnNewGame = new JButton("NEW GAME");
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guiFrame.dispose();
+				new PokerDesign();
+			}
+		});
+		GridBagConstraints gbc_btnNewGame = new GridBagConstraints();
+		gbc_btnNewGame.gridx = 0;
+		gbc_btnNewGame.gridy = 5;
+		evaluateButtonPanel.add(btnNewGame, gbc_btnNewGame);
+
+		guiFrame.getContentPane().add(picPanel);
+
+		JPanel allButtons = new JPanel();
+		GridBagConstraints gbc_allButtons = new GridBagConstraints();
+		gbc_allButtons.fill = GridBagConstraints.BOTH;
+		gbc_allButtons.gridx = 0;
+		gbc_allButtons.gridy = 2;
+		subPicpanel.add(allButtons, gbc_allButtons);
+
+		JButton raiseButton = new JButton("RAISE");
+		allButtons.add(raiseButton);
+
+		JButton showButton = new JButton("SHOW");
+		allButtons.add(showButton);
+
+		JButton foldButton = new JButton("FOLD");
+		allButtons.add(foldButton);
+		guiFrame.getContentPane().add(listPanel);
 		guiFrame.setVisible(true);
 
 	}
