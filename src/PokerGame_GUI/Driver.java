ed469c564fb1974866c63a35ace4ca12ad4a9a60
kotinarupali <33444113+kotinarupali@users.noreply.github.com>
2018-12-02 12:31:14 -0500

    Update Driver.java
    Adding the login page

--- src/PokerGame_GUI/Driver.java
+++ src/PokerGame_GUI/Driver.java
@@ -8,9 +8,11 @@ public class Driver {
 //Note: Typically the main method will be in a
 //separate class. As this is a simple one class
 //example it's all in the one class.
-public static void main(String[] args) {
-new PokerDesign();
-}
+	public static void main(String[] args) {
+		
+		LoginPage lp = new LoginPage();
+                lp.setVisible(true);
+		
+	}
 
 }
-
