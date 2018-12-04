32119e1841aff3dd2e93ccea894656ff9e41fa5b
naibingj <naibingj@buffalo.edu>
2018-12-04 10:15:01 -0500

    Add files via upload
    Unit Test Updated

--- CPUwinsTest.java
+++ CPUwinsTest.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package PokerGame_GUI;
+package AllTestsCases;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import PokerGame_GUI.Evaluate;
 
 /**
  *
@@ -20,8 +22,8 @@ public class CPUwinsTest {
         ArrayList<String> player_labels = new ArrayList<>(Arrays.asList("10C", "9C", "10D", "9D", "10H"));
         
         if(ev.evaluateHand(cpu_labels) > ev.evaluateHand(player_labels))
-            System.out.println("CPU wins");
+            System.out.println("CPU Wins!!");
         else
-            System.out.println("Player Wins");
+            System.out.println("Player Wins!!");
     }
 }
--- PlayerWinsTest.java
+++ PlayerWinsTest.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package PokerGame_GUI;
+package AllTestsCases;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import PokerGame_GUI.Evaluate;
 
 /**
  *
